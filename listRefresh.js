0dfebf778f6839e68147fb5ccdb1e32af32e4dab
enurseitov <enurseitov@users.noreply.github.com>
2018-11-29 13:50:20 +0100

    Update listRefresh.js

--- listRefresh.js
+++ listRefresh.js
@@ -1,7 +1,15 @@
 console.info("Start");
+const checkInterval = 3*1000
 
 function timerFunc() {
   console.info("timer")
+  var btn = document.getElementsByClassName("sc-jwKygS bPNJDy");
+  if (btn.length > 0) {
+    console.error("Update button detected")  
+    console.error(btn[0])
+    btn[0].click()
+  }
+
 }
 
-setInterval(timerFunc,1000);
+setInterval(timerFunc,checkInterval);
